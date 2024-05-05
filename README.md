0b58039b0ff964461700ba48690c6d0af807cf44
dsinclair1 <168162768+dsinclair1@users.noreply.github.com>
2024-05-04 21:05:29 -0400

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sprint-4-project
+
+This is a practice project mainly made for the purpose of improving the creator's proficiency with a number of programming tools. Plotly.express and streamlit feature heavily to present the project and show the charts used in it.
+
+Other modules used are Pandas and Altair
---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+cars = pd.read_csv(r"C:\Personal Files\Coding\GitHub\sprint-4-project\vehicles_us.csv")
+
+st.header('price by mileage')
+mp_hist = px.histogram(cars, x='price', y='odometer')
+st.write(mp_hist)
+
+st.header('price by type')
+type_hist = px.histogram(cars, x='price', y='type')
+st.write(type_hist)
+
+toggle = st.checkbox('Show model year', value=False)
+if toggle:
+    st.header('price by year')
+    year_scatt = px.scatter(cars, x='price', y='days_listed', color='model_year')
+    st.write(year_scatt)
+else:
+    st.header('price by list time')
+    time_hist = px.histogram(cars, x='price', y='days_listed')
+    st.write(time_hist)
