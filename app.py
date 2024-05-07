4f3b3dbb961707ac46b87a642133858592282f8c
dsinclair1 <168162768+dsinclair1@users.noreply.github.com>
2024-05-06 21:25:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from pathlib import Path
 
-cars = pd.read_csv(r"\vehicles_us.csv")
+cars = pd.read_csv(Path(r"C:\Personal Files\Coding\GitHub\sprint-4-project\vehicles_us.csv"))
 
 st.header('price by mileage')
 mp_hist = px.histogram(cars, x='price', y='odometer')
